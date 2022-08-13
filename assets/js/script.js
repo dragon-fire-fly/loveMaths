38cf03c9aa0e8a70fb8baf75bcb0042ecf74ea68
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-13 20:15:16 +0000

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,6 +88,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"]
     } else if (operator === "-"){
         return [operand1 - operand2, "subtract"]
+    } else if (operator === "/"){
+        return [operand1 / operand2, "division"]
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -132,7 +134,9 @@ function displayMultiplyQuestion(operand1, operand2){
 }
 
 function displayDivisionQuestion(operand1, operand2){
-
+    document.getElementById('operand1').textContent = (operand1 * operand2);
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
 
 
